3f9070dec9b69809b00128a73b8f6948bfcb3e4d
henrywu <henrywu@qnap.com>
2014-12-02 15:37:54 +0800

    fix infinite loop error

--- install.sh
+++ install.sh
@@ -41,9 +41,11 @@ echo ""
 echo "Your sachet has successfully been installed. Restart all Vim instances."
 echo "Thank you for using sachet"
 
-sudo apt-get install -y ctags cscope cmake 
+sudo apt-get install -y ctags cscope cmake python2.7-dev 
+
+vim -c q!
 
 if [ ! -f ~/.vim/bundle/YouCompleteMe/third_party/ycmd/ycm_client_support.so ]; then
-    cd ~/.vim/bundle/YouCompleteMe
-    ./install.sh --clang-completer
+    cd ~/.vim/bundle/YouCompleteMe && ./install.sh --clang-completer
 fi
+
